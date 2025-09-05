87d34e82aebb9a1a40871079cc3713c03a525880
Cesar Florian <ce.florian.27@gmail.com>
2025-09-04 23:35:36 -0600

    no message

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,14 +1,5 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   //empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
