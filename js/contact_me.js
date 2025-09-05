86dcdcea205a66bf1037e0837b549f4fb073cd43
Cesar Florian <ce.florian.27@gmail.com>
2025-09-05 00:26:21 -0600

    no message

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,13 +18,11 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: event.target.action,
+                type: form.method,
                 data: {
-                    name: name,
-                    phone: phone,
                     email: email,
-                    message: message
+                    message: message + ". Enviado por: " + name + (phone ?? ". Telefono: " + phone)
                 },
                 cache: false,
                 success: function() {
@@ -33,7 +31,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Tu mensaje ha sido enviado. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,7 +43,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Lo siento " + firstName + ",  parece que mi servidor de correo no responde. ¡Vuelve a intentarlo más tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
